282c61d6691288863f673cd1fd1574a257eb6716
BSeanSun <bs663@cornell.edu>
2016-08-08 16:09:39 +0000

    sendToServer.py

---
+++ sendToServer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from socket import *
+
+HOST = '192.168.1.3'
+PORT = 21811
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+print "client set"
+tcpCliSock.connect(ADDR)
+print "connection established"
+
+while True:
+    data = raw_input('>')
+    if not data:
+        break
+    tcpCliSock.send(data)
+    #data = tcpCliSock.recv(BUFSIZ)
+    #if not data:
+    #    break
+    #print data
+
+tcpCliSock.close()
+
