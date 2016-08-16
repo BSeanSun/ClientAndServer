f96d2a19d50863834094135add952fe843750814
BSeanSun <bs663@cornell.edu>
2016-08-16 16:56:49 +0000

    added ZichiDB obj for manipulate the sqlite database

--- recvClient.py
+++ recvClient.py
@@ -1,36 +1,46 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 from socket import *
 from time import ctime
+import ZichiDB
 #import sqlite3
 
 HOST = ''
-PORT = 12348
+PORT = 12349
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
 
 tcpSerSock = socket(AF_INET, SOCK_STREAM)
 
-tcpSerSock.settimeout(1)
+tcpSerSock.settimeout(10)
 tcpSerSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)
+zdb = ZichiDB.ZichiDB()
 
 while True:
     try:
         print 'waiting for connection'
-        tcpCliSock, addr = tcpSerSock.accept() #create and bind client
+
+        #create and bind client
+        tcpCliSock, addr = tcpSerSock.accept() 
         print '...connected from: ', addr
+
         while True:
             data = tcpCliSock.recv(BUFSIZ)
-            #if not data:
-            #	break
+            
+            #go back to the first while loop
+            if not data:
+            	break
             #tcpCliSock.send('[%s] %s' % (ctime(), data))
+            zdb.insert(data)
+            zdb.printdata();
             print data
             #else:
             #sentData = 'get'
             #if not sentData:
             #    break
-            tcpCliSock.send('sentData')
+            tcpCliSock.send(zdb.send())
     except KeyboardInterrupt:	
         tcpCliSock.close()
         tcpSerSock.close()
