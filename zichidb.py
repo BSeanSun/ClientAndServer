7a9d525e635fc0c098a5051ace2947b4d6d0308e
BSeanSun <bs663@cornell.edu>
2016-08-18 23:16:54 +0000

    db func revised, added time

--- zichidb.py
+++ zichidb.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+from time import ctime
 
 class zichidb:
     """create and manipulate the DB"""
@@ -11,11 +12,12 @@ class zichidb:
         #c = conn.cursor()
         #if os.path.isfile('zichi.db') == False:
         self.c.execute('''CREATE TABLE IF NOT EXISTS thidata
-	                 (temp real, humidity real, illum real)''')
+	                 (time text, temp real, humidity real, illum real)''')
         
-    def insert(self, thi):
-        temp, humid, illum = thi.split(' ')
-        self.c.execute("INSERT INTO thidata VALUES (?,?,?)", (temp, humid, illum))
+    def insert(self, data):
+        #thi = data.split(' ')
+        illum, temp, humid = data[0],data[1]/100.0,data[2]/100.0
+        self.c.execute("INSERT INTO thidata VALUES (?,?,?,?)", (ctime(), illum, temp, humid))
 
     def printdata(self):
         
