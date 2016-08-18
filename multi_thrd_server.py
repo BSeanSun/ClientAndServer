4ac25eb528630f9101fe24b7a16176fb602f9707
BSeanSun <bs663@cornell.edu>
2016-08-18 16:31:39 +0000

    first try for multi_thrd programming

---
+++ multi_thrd_server.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from socket import *
+from time import ctime
+import zichidb
+from thread import *
+
+HOST = ''
+PORT = 12343
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+
+#function for handling multi-connections
+def clientthrd(conn):
+    #conn.send('connected to %s',HOST)
+
+    while True:
+        data = conn.recv(1024)
+        print data
+        reply = 'OK...' + data
+        if not data:
+            break
+        conn.sendall(reply)
+    conn.close()
+
+#tcpSerSock.settimeout(10)
+tcpSerSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
+zdb = zichidb.zichidb()
+
+while True:
+    try:
+        print 'waiting for connection'
+
+        #create and bind client
+        #tcpCliSock, addr = tcpSerSock.accept() 
+        #print '...connected from: ', addr
+
+        while True:
+            tcpCliSock, addr = tcpSerSock.accept() 
+            print '...connected from: ', addr
+            data = tcpCliSock.recv(BUFSIZ)
+            start_new_thread(clientthrd, (tcpCliSock,))
+            #go back to the first while loop
+            #if not data:
+            #	break
+            #tcpCliSock.send('[%s] %s' % (ctime(), data))
+            #print data
+            #zdb.insert(data)
+            #zdb.printdata();
+            
+            #else:
+            #sentData = 'get'
+            #if not sentData:
+            #    break
+            #cmd = zdb.send()
+            #tcpCliSock.sendall(cmd)
+    except KeyboardInterrupt:	
+        tcpCliSock.close()
+        tcpSerSock.close()
+        exit(0)
