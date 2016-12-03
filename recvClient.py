87265d237020162ab0b603d1d09391a413a1cf94
BSeanSun <bs663@cornell.edu>
2016-12-03 02:23:01 +0000

    updated

--- recvClient.py
+++ recvClient.py
@@ -1,12 +1,16 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 from socket import *
-#from time import ctime
-import zichidb
-import realtimePlot
+import mysqlDB
+import time
+#import realtimePlot
+#import MySQLdb
+import emailAlert
+
+
 
 HOST = ''
-PORT = 11245
+PORT = 11246
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
 
@@ -16,9 +20,9 @@ tcpSerSock.settimeout(10)
 tcpSerSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)
-zdb = zichidb.zichidb()
-xList = [0]
-yList = [0]
+zdb = mysqlDB.mysqlDB()
+#xList = [0]
+#yList = [0]
 def Byte2Hex(byteStr):
     return ' '.join(["%02X" % ord(x) for x in byteStr]).strip()
 
@@ -31,54 +35,40 @@ while True:
         print '...connected from: ', addr
         #plt.draw()
         while True:
+            
             data = tcpCliSock.recv(BUFSIZ)
             #go back to the first while loop
             if not data:
             	break
                         
             #print data.decode("hex")
-            decodedHex = Byte2Hex(data)
+            decodedHex = Byte2Hex(data) #Byte2Hex func
             
-            hexToInt = decodedHex.split(' ')
-            print hexToInt
-            thi = []
+            hexToInt = decodedHex.split(' ') #Hex2Int and split temp, hum, illm
+            
+            thi = [] #refreshed for each data stream
             for i in xrange(3,len(hexToInt)-3,2):
                 res = int(hexToInt[i],16)*256 + int(hexToInt[i+1],16)
-                print res
+                #print res
                 thi.append(res)
-            if thi[0] < 300:
-                print thi[0]  
+
+            #call email alert func
+            if thi[1] < 0:
+                emailAlert.alert() 
             
             if not data:
                 break
-            yList.append(thi[1]/100.0)
-            xList.append(xList[-1] + 1)
-            realtimePlot.realtimePlot(xList[-10:-1], yList[-10:-1])
+            #yList.append(thi[1]/100.0)  #y-axis
+            #xList.append(xList[-1] + 1) #x-axis
+            #realtimePlot.realtimePlot(xList[-10:-1], yList[-10:-1])  #draw plot
              
             #    print int(hex16,16)
                 #hexToInt = hexToInt.append(int(hex16,16))
+            
             zdb.insert(thi)
             zdb.printdata()
-            
-            #if zdb.checkTemp() > 30:
-            """if True:
-                HOST = '192.168.1.5'
-                PORT = 12338
-                BUFSIZ = 1024
-                ADDR = (HOST, PORT)
-
-                rptorelay = socket(AF_INET, SOCK_STREAM)
-                print "client set"
-                rptorelay.connect(ADDR)
-                print "connection established"  
-                rptorelay.send('hlkATat+GW=0,0 \r')  
-                rptorelay.close()   """ 
-            #else:
-            #sentData = 'get'
-            #if not sentData:
-            #    break
-            #cmd = zdb.send()
-            #tcpCliSock.sendall(cmd)
+            time.sleep(10)
+           
 #except KeyboardInterrupt:	
     finally:
         #rptorelay.close()
